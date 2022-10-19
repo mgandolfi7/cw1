557854899018cb29ba9ca99a065c9855550a304a
Michael <michaelgandolfi7@gmail.com>
2022-10-19 14:08:39 -0500

    04 complete

--- js/main.js
+++ js/main.js
@@ -8,9 +8,27 @@
 
 // arr(20)
 
-//02
-class Kata {
-    static getVolumeOfCuboid(length, width, height) {
-      return length * width * height
+// //02
+// class Kata {
+//     static getVolumeOfCuboid(length, width, height) {
+//       return length * width * height
+//     }
+//   }
+
+  //03 
+//   function strCount(str, letter){  
+//     return str.split(letter).length-1
+//   }
+
+//   console.log(strCount("hello", "l"))
+
+//04
+var summation = function (num) {
+    let sum = 0;
+    for (i = 1; i <= num; i++) {
+      sum += i
     }
+    return sum
   }
+
+  console.log(summation(6))
