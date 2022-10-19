d4af454f3c1c93d38527e3bebc3f51ee7d38222f
Michael <michaelgandolfi7@gmail.com>
2022-10-19 14:01:07 -0500

    challenge 2 completed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-function arr(n) {
-    newArr = [];
-    for (i = 1; i <= n; i++) {
-        newArr.push(i)
-    }
-}
+//01
+// function arr(n) {
+//     newArr = [];
+//     for (i = 1; i <= n; i++) {
+//         newArr.push(i)
+//     }
+// }
 
-arr(20)
+// arr(20)
+
+//02
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height
+    }
+  }
